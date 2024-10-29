9be3df9524b9cc5b13fb38c3cf0a4e57ccee878c
holy-jesus <hj@holy-coder.ru>
2024-10-29 19:35:15 +0500

    Немного переделал TaskBar; Сделал StartButton

--- src/components/StartButton.vue
+++ src/components/StartButton.vue
@@ -1,7 +1,34 @@
 <script setup lang="ts">
+import { ref } from "vue";
 
+const hover = ref(false);
+const pressed = ref(false);
 </script>
 
 <template>
-    
+    <div class="h-full w-[99px] overflow-hidden" @mouseover="hover = true" @mouseleave="hover = false"
+        @mousedown="pressed = !pressed">
+        <TransitionGroup name="startbuttonbackground">
+            <div class="absolute h-full w-[99px]" style="background-image: url(/src/assets/startbuttondefault.png);">
+            </div>
+            <div class="absolute h-full w-[99px]" style="background-image: url(/src/assets/startbuttonhover.png);"
+                v-if="hover && !pressed">
+            </div>
+            <div class="absolute h-full w-[99px]" style="background-image: url(/src/assets/startbuttonpressed.png);"
+                v-if="pressed">
+            </div>
+        </TransitionGroup>
+    </div>
 </template>
+
+<style lang="css" scoped>
+.startbuttonbackground-enter-active,
+.startbuttonbackground-leave-active {
+    transition: opacity 0.1s ease;
+}
+
+.startbuttonbackground-enter-from,
+.startbuttonbackground-leave-to {
+    opacity: 0;
+}
+</style>
--- src/components/TaskBar.vue
+++ src/components/TaskBar.vue
@@ -1,27 +1,22 @@
 <script setup lang="ts">
+import StartButton from "./StartButton.vue"
+
 
 </script>
 
 <template>
     <div class="task-bar w-screen">
-        
+        <StartButton />
     </div>
 </template>
 
 <style scoped>
 .task-bar {
     font-family: "Trebuchet MS";
-    background: linear-gradient(180deg,
-            rgba(57, 103, 219, 1) 0%,
-            rgba(83, 147, 235, 1) 10%,
-            rgba(46, 91, 221, 1) 20%,
-            rgba(44, 84, 220, 1) 40%,
-            rgba(34, 87, 213, 1) 60%,
-            rgba(34, 87, 213, 1) 80%,
-            rgba(30, 61, 170, 1) 100%
-        );
-    padding: 3px 5px 3px 3px;
+    background-image: url("/src/assets/taskbarbackground.png");
+    background-size: contain;
+    
     font-size: 13px;
-    height: 30px;
+    height: 31px;
 }
 </style>
