d2f3bcd16fdc5d3448c1fde88047fdca9f9aaaf4
holy-jesus <hj@holy-coder.ru>
2024-10-30 06:44:34 +0500

    Сделал тестовую иконку; Добавил курсор

---
+++ src/components/App.vue
@@ -0,0 +1,11 @@
+<script setup lang="ts">
+defineProps({ id: String, icon: String, name: String })
+
+</script>
+
+<template>
+    <div class="absolute max-w-20 flex flex-col justify-center items-center select-none gap-1" draggable="true">
+        <img class="slight-shadow h-8" :src="icon" draggable="false">
+        <div class="slight-shadow text-white text-[16px] text-center leading-4">{{ name }}</div>
+    </div>
+</template>
--- src/components/Desktop.vue
+++ src/components/Desktop.vue
@@ -1,8 +1,10 @@
 <script setup lang="ts">
-
+import App from './App.vue';
 
 </script>
 
 <template>
-    <div class="flex-grow"></div>
+    <div class="flex-grow p-4">
+        <App id="youtube" icon="/src/assets/youtube-icon.png" name="Youtube Downloader" />
+    </div>
 </template>
